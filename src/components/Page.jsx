5fb5d8a3dcfd79854ce836d4f6261294bcc1974a
sanivarapun <nithishkumar.sanivarapu@xelpmoc.in>
2024-02-01 16:42:42 +0530

    search params used

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,13 +1,17 @@
 /** @format */
 
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export default function Page() {
   const { id, index } = useParams();
+  const queryparams = useLocation();
+  const TotalQueryParams = new URLSearchParams(queryparams.search);
+  console.log(TotalQueryParams);
+
   return (
     <h1>
-      Page has id {id} and index {index}
+      Page has id {id} and index {index} posts {TotalQueryParams.get("posts")} and category is {TotalQueryParams.get("category")}
     </h1>
   );
 }
