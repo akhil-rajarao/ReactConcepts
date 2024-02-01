04180dccbc219cc13ee6647035929b626a3f6eab
sanivarapun <nithishkumar.sanivarapu@xelpmoc.in>
2024-02-01 18:05:35 +0530

    added use ref to home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,26 @@
 /** @format */
 
-import React from "react";
+import React, { useRef } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function Home() {
+  const ele = useRef(null);
   return (
     <>
-      <h1>Home</h1>
+      <h1 ref={ele}>Home</h1>
+      <button
+        onClick={() => {
+          ele.current.classList.toggle("text-danger");
+        }}>
+        Change Style
+      </button>
 
-      <Link to="left"><button>Left</button></Link>
-      <Link to="right"><button>Right</button></Link>
+      <Link to="left">
+        <button>Left</button>
+      </Link>
+      <Link to="right">
+        <button>Right</button>
+      </Link>
       <Outlet />
     </>
   );
