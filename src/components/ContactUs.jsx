dd21cb37713afc4f3494bf5134a86ee18bf97fa5
sanivarapun <nithishkumar.sanivarapu@xelpmoc.in>
2024-02-05 14:14:16 +0530

    props used in contactus component using usecontext

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+/** @format */
+
+import React, { useContext } from "react";
+import { context } from "../App";
 
 export default function ContactUs() {
+  const prod = useContext(context);
   return (
-    <h1>ContactUs</h1>
-  )
+    <>
+      {" "}
+      <h1>ContactUs</h1>
+      <p>{prod.price}</p>
+    </>
+  );
 }
