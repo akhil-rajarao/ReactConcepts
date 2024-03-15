1540dda3a077b4c1885c6e2a2f43f617aaa4b78f
akhil <akhil.rajarao@xelpmoc.in>
2024-03-15 12:29:38 +0530

    added counter in contact page

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,15 +1,19 @@
 /** @format */
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { context } from "../App";
 
 export default function ContactUs() {
   const prod = useContext(context);
+  const [count, setCount] = useState(0);
   return (
     <>
       {" "}
       <h1>ContactUs</h1>
       <p>{prod.price}</p>
+      <p>Counter {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button  onClick={() => setCount(0)}>reset</button>
     </>
   );
 }
